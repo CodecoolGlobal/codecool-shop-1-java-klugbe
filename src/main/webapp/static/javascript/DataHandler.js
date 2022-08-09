db326ed55f0633019ce382e40f18a4c972b7305e
Vitályos Márton <marci12345678956@gmail.com>
2022-08-09 09:54:38 +0200

    Add javascript directory to static directory and DataHandler.js within the javascript directory

---
+++ src/main/webapp/static/javascript/DataHandler.js
@@ -0,0 +1,32 @@
+
+    export const dataHandler = {
+
+    }
+
+
+    async function apiGet(url) {
+        const response = await fetch(url, {
+            method : "GET",
+        });
+        if(response.status < 300) {
+            return response.json();
+        }
+    }
+
+
+    async function apiPost(url, data) {
+        const response = await fetch(url, {
+            method : "POST",
+            headers : {
+                "Content-type" : "application/json"
+            },
+            body : JSON.stringify(data)
+        });
+        return await response.json();
+    }
+
+    async function apiDelete(url) {
+        await fetch(url, {
+            method: "DELETE"
+        });
+    }
