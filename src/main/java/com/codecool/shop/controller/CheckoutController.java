c3d0ea0afff046cd236047a33dcbfba9a95b0d92
Vitályos Márton <marci12345678956@gmail.com>
2022-08-09 11:44:42 +0200

    Add CheckoutController doGet method's body

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,5 +1,10 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +17,9 @@ public class CheckoutController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        TemplateEngine templateEngine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext webContext = new WebContext(request,response, getServletContext());
+        templateEngine.process("checkout.html", webContext, response.getWriter());
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
