1e907bddee28ac197ef462ac768be3a6d02d1319
Vitályos Márton <marci12345678956@gmail.com>
2022-08-09 11:20:31 +0200

    Add CheckoutControllel and checkout html template

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Checkout", urlPatterns = {"/checkout"})
+public class CheckoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
