2bf76808f1f8717222de94aaff7831e32d98598b
klugbe <90328917+klugbe@users.noreply.github.com>
2022-08-11 16:14:17 +0200

    add cart dao

--- src/main/java/com/codecool/shop/dao/CartDao.java
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -1,9 +1,15 @@
 package com.codecool.shop.dao;
 
-import com.codecool.shop.model.Product;
-import java.util.Map;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.CartItem;
 
 public interface CartDao {
-    void add(Product product, Integer quantity);
-    Map<Product, Integer> getAll();
+
+    void add(CartItem cartItem, String sessionId);
+
+    CartItem find(int id, String sessionId);
+
+    void remove(int id, String sessionId);
+
+    Cart getCart(String sessionId);
 }
